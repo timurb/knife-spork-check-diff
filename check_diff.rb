08fa8a51508d6dac7a275e16ee0cc4e231890ac1
Timur Batyrshin <erthad@gmail.com>
2017-07-27 12:54:28 +0300

    fix rubocop warnings

--- check_diff.rb
+++ check_diff.rb
@@ -12,7 +12,9 @@ module KnifeSpork
     class CheckDiff < Plugin
       name :checkdiff
 
-      def perform; raise end
+      def perform
+        raise
+      end
 
       # Environmental Git wrappers
       def before_environmentcreate
@@ -40,44 +42,42 @@ module KnifeSpork
           local_environment = env.to_hash
           remote_environment = load_remote_environment(env.name).to_hash
           diff = HashDiff.diff(remote_environment, local_environment)
-          if !diff.empty?
-            message = "Local environment is different from remote.\nAttributes changed:\n#{format_diff(diff)}"
-            if config.epic_fail
-              ui.error message
-              ui.error 'Make sure environments match each other before proceeding'
-              exit 1
-            else
-              ui.warn message
-            end
+          next if diff.empty?
+
+          message = "Local environment is different from remote.\nAttributes changed:\n#{format_diff(diff)}"
+          if config.epic_fail
+            ui.error message
+            ui.error 'Make sure environments match each other before proceeding'
+            exit 1
+          else
+            ui.warn message
           end
         end
       end
 
       def check_environment_path(path)
-        if !File.directory?(path)
-          ui.error "Environment path #{path} does not exist"
-          exit 1
-        end
-       end
+        return if File.directory?(path)
+
+        ui.error "Environment path #{path} does not exist"
+        exit 1
+      end
 
       def save_environment(environment)
         json = JSON.pretty_generate(Chef::Environment.load(environment))
-        environment_file = File.expand_path( File.join(environment_path, "#{environment}.json") )
-        File.open(environment_file, 'w'){ |f| f.puts(json) }
+        environment_file = File.expand_path(File.join(environment_path, "#{environment}.json"))
+        File.open(environment_file, 'w') { |f| f.puts(json) }
       end
 
       def delete_environment(environment)
-        environment_file = File.expand_path( File.join(environment_path, "#{environment}.json") )
+        environment_file = File.expand_path(File.join(environment_path, "#{environment}.json"))
         File.delete(environment_file)
       end
 
       def load_remote_environment(environment_name)
-        begin
-          Chef::Environment.load(environment_name)
-        rescue Net::HTTPServerException => e
-          ui.error "Could not load #{environment_name} from Chef Server. You must upload the environment manually the first time."
-          exit(1)
-        end
+        Chef::Environment.load(environment_name)
+      rescue Net::HTTPServerException
+        ui.error "Could not load #{environment_name} from Chef Server. You must upload the environment manually the first time."
+        exit(1)
       end
 
       def format_diff(diff)
